fe39a73f97ca17f3d36878a7fbbce258fdfa4543
박경현 <78902671+GoldenPig1205@users.noreply.github.com>
2024-12-06 15:14:19 +0900

    v1.1.0 릴리즈

--- DiscordInteraction.cs
+++ DiscordInteraction.cs
@@ -20,7 +20,7 @@ namespace DiscordInteraction
 
         public override string Name => "DiscordInteraction";
         public override string Author => "GoldenPig1205";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 1, 0);
         public override Version RequiredExiledVersion => new Version(1, 2, 0, 5);
 
         public override void OnEnabled()
